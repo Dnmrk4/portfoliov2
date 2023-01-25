7d43358145dee5823c5d8b5ae8aeace2898ef0f8
Dnmrk4 <danmark.chemuren@gmail.com>
2023-01-25 15:14:55 +0300

    deleted projects tsx

--- src/pages/experience.tsx
+++ src/pages/experience.tsx
@@ -3,7 +3,6 @@ import IndexPage from "../components/title";
 import Link from "next/link";
 import Image from "next/image";
 import Line from "../components/Line";
-import Projects from "../components/Projects";
 
 function Experience(): JSX.Element {
   return (
@@ -129,7 +128,19 @@ function Experience(): JSX.Element {
         <Line />
         <div>
           <div className="items-center p-2 mx-auto ">
-            <Projects />
+            <div className="flex flex-col pt-2 mb-12 md:pt-32 md:mb-24 md:p-24 ">
+              <h3 className="pt-2 mb-12 text-2xl text-center md:mb-24 font-head1kod">Projects</h3>
+              <div className="flex flex-col mx-auto space-x-32 md:flex-row font-text2 ">
+                <div className="w-auto p-4">
+                  <Link href="https://portfoliov1.netlify.com/"><Image className="w-full rounded-4xl " src="/assets/porto.jpeg" width="244" height="140" alt="" /></Link>
+                  <Link href="https://portfoliov1.netlify.com/"><h5 className="p-3 text-sm italic text-center text-current md:text-left">Portfolio v1</h5></Link>
+                </div>
+                <div className="w-auto p-4">
+                  <Link href="/gallery#marcus"><Image className="w-full rounded-4xl " src="/assets/KFK.png" width="244" height="140" alt="" /></Link>
+                  <Link href="/gallery#marcus"><h5 className="p-3 text-sm italic text-center text-current md:text-left">Marcus THY Rapper</h5></Link>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
