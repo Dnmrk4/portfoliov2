ed6bbced9a98742d628e451ee2c53c46229219a2
Dnmrk4 <danmark.chemuren@gmail.com>
2023-07-04 02:08:52 +0300

    webconfig

--- next.config.js
+++ next.config.js
@@ -2,16 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.pdf$/,
-      use: {
-        loader: 'file-loader',
-        options: {
-          name: '[path][name].[ext]',
-        },
-      },
-    });
-  }}
+
+  }
 
 module.exports = nextConfig
